a31774822d85fc12cd9321e085432bab500e5492
1042545965@qq.com <dkz13887548339>
2022-06-06 23:26:29 +0800

    springboot 2.7 整合 redis lettuce sentinel 模式  添加了 redisUtils 操作

--- src/test/java/com/redis/study/RedisStudyDemoApplicationTests.java
+++ src/test/java/com/redis/study/RedisStudyDemoApplicationTests.java
@@ -1,29 +1,20 @@
 package com.redis.study;
 
-import com.redis.study.config.RedisClient;
+import com.redis.study.config.RedisUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 class RedisStudyDemoApplicationTests {
 
     @Autowired
-    RedisClient redisClient;
+    RedisUtils redisUtils;
 
-    @Autowired
-    StringRedisTemplate stringRedisTemplate;
     @Test
     void contextLoads() {
-        try {
-            System.out.println(redisClient.get("dkz_test"));
-            ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
-            System.out.println(ops.get("dkz_test"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        RedisUtils.set("redisUtils", "redisUtils");
+        System.out.println(RedisUtils.get("redisUtils"));
     }
 
 }
